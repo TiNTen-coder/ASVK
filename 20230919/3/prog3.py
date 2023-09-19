1816de08d96741a46d3cdeb8821e6e484b28b5d1
Vyacheslav Kret <kretva@my.msu.ru>
2023-09-19 15:57:04 +0300

    Homework task 3

---
+++ 20230919/3/prog3.py
@@ -0,0 +1,16 @@
+def digits_sum(n):
+	ans = 0	
+	while n:
+		ans += n % 10
+		n //= 10
+	return ans
+
+n = int(input())
+for i in range(n, n + 3):
+	for j in range(n, n + 3):
+		if digits_sum(i * j) == 6:
+			s = ":=)"
+		else:
+			s = i * j
+		print(f'{i} * {j} = {s}', end = ' ')
+	print()
