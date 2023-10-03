cee43ae506a6919f2cbb26756d0e2d29f69b55b2
Vyacheslav Kret <kretva@my.msu.ru>
2023-10-03 14:53:11 +0300

    Homework 4 task 1

--- 20231003/1/prog1.py
+++ 20231003/1/prog1.py
@@ -1,4 +1,6 @@
 def Pareto(*args):
+    if type(args[0]) == int:
+        return args
     ans = []
     for i in range(len(args)):
         flag = True
@@ -10,8 +12,4 @@ def Pareto(*args):
 
     return tuple(ans)
 
-
-print(Pareto((32, 38), (10, 14), (19, 44), (31, 31), (17, 33), (53, 6), (48, 9), (6, 38), (30, 49), (52, 30), (7, 30), (45, 45), (21, 51), (7, 49), (11, 23)))
-
-print(Pareto((1,2), (3,4), (2,2), (4,3), (7,0), (1,8)))
-
+print(tuple(Pareto(*eval(input()))))
